e749efb58d552291737f506816c208c3172d4392
UgiNyaa <ugi@waifu.club>
2018-04-23 10:00:22 -0500

    IpcHandler now successfully handles JSON messages
    With a complex match structure, JSON messages are parsed and its representation is contained in three values:
    - method: which method should be used, so what kind of data has to be queried or what kind of manipulation is requested on the ECS
    - id: the message ID decided by the client, in order to not shuffle up responses.
    - params: The parameters for the method explained above.
    
    For now, errors are handles with panics, which is basically not a good idea. However, priority is also low in implementing that.

--- square-backend/src/systems/ipc_handler.rs
+++ square-backend/src/systems/ipc_handler.rs
@@ -29,6 +29,49 @@ impl<'a> System<'a> for IpcHandler {
     type SystemData = ();
 
     fn run(&mut self, (): Self::SystemData) {
+        match self.rx.try_recv() {
+            Ok(json) => match json.as_object() {
+                Some(map) => { 
+                    let method = match map.get("method") {
+                        Some(method) => match method.as_str() {
+                            Some(method) => method,
+                            None => panic!("value of 'method' key is not a string"),
+                        },
+                        None => panic!("There is not 'method' key"),
+                    };
+
+                    let id = match map.get("id") {
+                        Some(id) => match id.as_str() {
+                            Some(id) => id,
+                            None => panic!("value of 'id' key is not a string"),
+                        },
+                        None => panic!("There is not 'id' key"),
+                    };
+
+                    let params = match map.get("params") {
+                        Some(params) => match params.as_array() {
+                            Some(params) => params.iter()
+                                .map(|p| match p.as_str() {
+                                    Some(p) => p.to_string(),
+                                    None => panic!("Not all params are string types"),
+                                })
+                                .collect::<Vec<String>>(),
+                            None => panic!("value of 'params' key is not an array"),
+                        },
+                        None => panic!("There is not 'params' key"),
+                    };
+
+                    println!("method: {:?}, id: {:?}, params: {:?}", method, id, params);
+                },
+                None => panic!("JSON message is not an object"),
+            },
+            Err(e) => match e {
+                TryRecvError::Empty => return,
+                TryRecvError::Disconnected => panic!("Cannot receive input: {}", e),
+            },
+        };
+
+/*
         let result = self.rx.try_recv();
 
         if let Err(e) = result {
@@ -41,5 +84,6 @@ impl<'a> System<'a> for IpcHandler {
         if let Ok(json) = result {
             println!("input received: {:?}", json);
         }
+*/
     }
 }
